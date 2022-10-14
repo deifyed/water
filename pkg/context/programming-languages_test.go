f2137ad86fb3cd6fc758f2434d0b87d05ba0ac2c
Julius Pedersen <deifyed@tutanota.com>
2022-10-14 21:58:43 +0200

    ✅ Add failing main language for dir tests

--- pkg/context/programming-languages_test.go
+++ pkg/context/programming-languages_test.go
@@ -55,3 +55,94 @@ func TestAcquireMainLanguageForFile(t *testing.T) {
 		})
 	}
 }
+
+func TestAcquireMainLanguageForDir(t *testing.T) {
+	testCases := []struct {
+		name           string
+		withFiles      []testFile
+		expectLanguage string
+	}{
+		{
+			name: "Should return golang with only golang files",
+			withFiles: []testFile{
+				{
+					Path:    "/main.go",
+					Content: strings.NewReader("package main"),
+				},
+				{
+					Path:    "/tests.go",
+					Content: strings.NewReader("package main"),
+				},
+				{
+					Path:    "/types.go",
+					Content: strings.NewReader("package main"),
+				},
+			},
+			expectLanguage: "golang",
+		},
+		{
+			name: "Should return golang with mostly golang files",
+			withFiles: []testFile{
+				{
+					Path:    "/main.go",
+					Content: strings.NewReader("package main"),
+				},
+				{
+					Path:    "/tests.go",
+					Content: strings.NewReader("package main"),
+				},
+				{
+					Path:    "/Makefile",
+					Content: strings.NewReader(".PHONY: all"),
+				},
+			},
+			expectLanguage: "golang",
+		},
+		{
+			name: "Should return golang with 3/5 files being golang",
+			withFiles: []testFile{
+				{
+					Path:    "/monitor.java",
+					Content: strings.NewReader("public static void main() {}"),
+				},
+				{
+					Path:    "/main.go",
+					Content: strings.NewReader("package main"),
+				},
+				{
+					Path:    "/tests.go",
+					Content: strings.NewReader("package main"),
+				},
+				{
+					Path:    "/types.go",
+					Content: strings.NewReader("package main"),
+				},
+				{
+					Path:    "/lib.java",
+					Content: strings.NewReader("public static void main() {}"),
+				},
+			},
+			expectLanguage: "golang",
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			fs := &afero.Afero{Fs: afero.NewMemMapFs()}
+
+			for _, item := range tc.withFiles {
+				err := fs.WriteReader(item.Path, item.Content)
+				assert.NoError(t, err)
+			}
+
+			language, err := acquireMainLanguageForDir(fs, "/")
+			assert.NoError(t, err)
+
+			assert.Equal(t, tc.expectLanguage, language)
+		})
+	}
+}
