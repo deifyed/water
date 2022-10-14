25f58a8b21c1b66298a76e7c9becbb75d6e455a6
Julius Pedersen <deifyed@tutanota.com>
2022-10-14 21:43:53 +0200

    ✅ Add more Discover tests

--- pkg/template/api_test.go
+++ pkg/template/api_test.go
@@ -19,7 +19,7 @@ func TestDiscover(t *testing.T) {
 		expectTemplate  string
 	}{
 		{
-			name: "Should work",
+			name: "Should find relevant file",
 			withContext: context.Context{
 				TargetType: context.TargetTypeFile,
 				TargetPath: "/Makefile",
@@ -47,6 +47,95 @@ func TestDiscover(t *testing.T) {
 				assert.NoError(t, err)
 
 				err = fs.WriteFile("/templates/Makefile/golang", []byte("golang makefile content"), 0o600)
+				assert.NoError(t, err)
+
+				return fs
+			}(),
+			withTemplateDir: "/templates",
+			expectTemplate:  "golang makefile content",
+		},
+		{
+			name: "Should find relevant file when same file alternatives exist",
+			withContext: context.Context{
+				TargetType: context.TargetTypeFile,
+				TargetPath: "/Makefile",
+				Tags:       map[string]string{"language": "golang"},
+			},
+			withFs: func() *afero.Afero {
+				fs := &afero.Afero{Fs: afero.NewMemMapFs()}
+
+				err := fs.MkdirAll("/templates/Makefile", 0o700)
+				assert.NoError(t, err)
+
+				metadatas := []metadata{
+					{Target: "./golang", Tags: map[string]string{"language": "golang"}},
+					{Target: "./java", Tags: map[string]string{"language": "java"}},
+				}
+
+				raw, err := json.Marshal(metadatas)
+				assert.NoError(t, err)
+
+				err = fs.WriteFile("/templates/Makefile/metadata", raw, 0o600)
+				assert.NoError(t, err)
+
+				err = fs.WriteFile("/templates/Makefile/golang", []byte("golang makefile content"), 0o600)
+				assert.NoError(t, err)
+
+				err = fs.WriteFile("/templates/Makefile/java", []byte("java makefile content"), 0o600)
+				assert.NoError(t, err)
+
+				return fs
+			}(),
+			withTemplateDir: "/templates",
+			expectTemplate:  "golang makefile content",
+		},
+		{
+			name: "Should find relevant file when same file alternatives exist and other templates",
+			withContext: context.Context{
+				TargetType: context.TargetTypeFile,
+				TargetPath: "/Makefile",
+				Tags:       map[string]string{"language": "golang"},
+			},
+			withFs: func() *afero.Afero {
+				fs := &afero.Afero{Fs: afero.NewMemMapFs()}
+
+				err := fs.MkdirAll("/templates/Makefile", 0o700)
+				assert.NoError(t, err)
+
+				metadatas := []metadata{
+					{Target: "./golang", Tags: map[string]string{"language": "golang"}},
+					{Target: "./java", Tags: map[string]string{"language": "java"}},
+				}
+
+				raw, err := json.Marshal(metadatas)
+				assert.NoError(t, err)
+
+				err = fs.WriteFile("/templates/Makefile/metadata", raw, 0o600)
+				assert.NoError(t, err)
+
+				err = fs.WriteFile("/templates/Makefile/golang", []byte("golang makefile content"), 0o600)
+				assert.NoError(t, err)
+
+				err = fs.WriteFile("/templates/Makefile/java", []byte("java makefile content"), 0o600)
+				assert.NoError(t, err)
+
+				err = fs.MkdirAll("/templates/package.json", 0o700)
+				assert.NoError(t, err)
+
+				metadatas = []metadata{
+					{
+						Target: "./vue",
+						Tags:   map[string]string{"language": "javascript", "framework": "vue"},
+					},
+				}
+
+				raw, err = json.Marshal(metadatas)
+				assert.NoError(t, err)
+
+				err = fs.WriteFile("/templates/package.json/metadata", raw, 0o600)
+				assert.NoError(t, err)
+
+				err = fs.WriteFile("/templates/package.json/vue", []byte("vue package.json content"), 0o600)
 				assert.NoError(t, err)
 
 				return fs
